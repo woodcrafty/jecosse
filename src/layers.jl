e96d49a827e35d95a7e1888bfc2e1f74a1a6bc3b
Mark Richards <mark.l.a.richards@gmail.com>
2026-04-14 16:57:22 +0100

    Added function index_from_depth().

--- src/layers.jl
+++ src/layers.jl
@@ -1,4 +1,4 @@
-module Layers2
+# This file contains types and functions related to dealing with layers
 
 """
 - z1: Distance from the surface to the top of each layer [cm]
@@ -13,7 +13,8 @@ struct Layers
     dz::Vector{Float64}
 
     """
-    - dz_layers: Array specifying the depth (thickness) of each layer, starting from the surface [cm]
+    - dz_layers: Array of numbers specifying the depth (thickness) of each layer from [cm]
+        Layers are specified beginning with the layer closest to the surace and ending with the deepest layer
         e.g. [5, 5, 10] will produce the layers that look like this:
 
                 ---- 0 cm (surface)
@@ -35,9 +36,24 @@ struct Layers
     end
 end
 
+"""
+Return the index of the layer corresponding to a depth.
+
+If the depth does not correspond to any of the layers, the function returns nothing.
+"""
+function index_from_depth(lyrs::Layers, depth)
+    for i = 1:length(lyrs.z1)
+        if depth >= lyrs.z1[i] && depth < lyrs.z2[i]
+            return i
+        end
+        if depth == lyrs.z2[i]
+            return i
+        end
+    end
+    return nothing
+end
+
 #lyrs = Layers([5.0, 5.0, 10.0, 10.0])
 lyrs = Layers([5, 5, 10, 10])
 println(lyrs)
-println(lyrs.z1)
-
-end
+println(index_from_depth(lyrs, 12))
